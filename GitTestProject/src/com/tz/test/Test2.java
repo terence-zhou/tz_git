cce1b886985e44cc89cd7ebc8f9ebccff682dd2e
terence-zhou <magicmars@sina.com>
2017-08-29 15:58:21 +0800

    第三次提交

--- GitTestProject/src/com/tz/test/Test2.java
+++ GitTestProject/src/com/tz/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2
     public static void main(String[] args)
     {
         System.out.println("=========我是test2===========");
-        System.out.println(System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis()+"$$$$$$$$");
         System.out.println("=========我是test2===========");
     }
 }
