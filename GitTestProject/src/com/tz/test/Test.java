b684c4a90627cb0b912891e9f8a541eb37d74ae1
terence-zhou <magicmars@sina.com>
2017-08-29 15:57:19 +0800

    第二次提交

--- GitTestProject/src/com/tz/test/Test.java
+++ GitTestProject/src/com/tz/test/Test.java
@@ -1,10 +1,13 @@
+package com.tz.test;
 
 public class Test
 {
     
     public static void main(String[] args)
     {
+        System.out.println("========我是test============");
         System.out.println(System.currentTimeMillis());
+        System.out.println("========我是test============");
     }
     
 }
---
+++ GitTestProject/src/com/tz/test/Test2.java
@@ -0,0 +1,10 @@
+package com.tz.test;
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("=========我是test2===========");
+        System.out.println(System.currentTimeMillis());
+        System.out.println("=========我是test2===========");
+    }
+}
