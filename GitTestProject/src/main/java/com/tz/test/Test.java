d6efdc678a75764fc27721fc8d98734c6cfa1621
Administrator <Administrator@XYXW6F8ANSUITHR>
2017-09-03 14:52:58 +0800

    增加了注释

--- GitTestProject/src/main/java/com/tz/test/Test.java
+++ GitTestProject/src/main/java/com/tz/test/Test.java
@@ -5,6 +5,7 @@ public class Test
     
     public static void main(String[] args)
     {
+    	//Ôö¼Ó×¢ÊÍ
         System.out.println("========æˆ‘æ˜¯test============");
         System.out.println(System.currentTimeMillis());
         System.out.println("========æˆ‘æ˜¯test============");
