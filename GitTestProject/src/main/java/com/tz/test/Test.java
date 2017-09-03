fc0291944da4c0e0ccd9467b0411212f0d10e158
terence-zhou <magicmars@sina.com>
2017-09-03 16:37:14 +0800

    怎么会乱码

--- GitTestProject/src/main/java/com/tz/test/Test.java
+++ GitTestProject/src/main/java/com/tz/test/Test.java
@@ -5,7 +5,7 @@ public class Test
     
     public static void main(String[] args)
     {
-    	//Ôö¼Ó×¢ÊÍ
+    	//æ€ä¹ˆä¼šæœ‰ä¹±ç 
         System.out.println("========æˆ‘æ˜¯test============");
         System.out.println(System.currentTimeMillis());
         System.out.println("========æˆ‘æ˜¯test============");
